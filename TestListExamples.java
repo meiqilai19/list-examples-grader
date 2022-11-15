9357e44654c4f84f6b21da822291a95213d20d1d
meiqilai19 <111710803+meiqilai19@users.noreply.github.com>
2022-11-15 12:23:22 -0800

    b

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,4 +3,18 @@ import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void test1(){
+        StringChecker check=new myChecker();
+        List<String> input1=new ArrayList<>();
+        String[] expected={};
+        input1.add("");
+        assertArrayEquals(,ListExamples.filter(input1, check).toArray());
+        String[] expected={"hi", "hii", "hiii", "hiiii"};
+        input1.add("hi");
+        input1.add("hii");
+        input1.add("hiii");
+        input1.add("hiiii");
+        assertArrayEquals(expected,ListExamples.filter(input1, check).toArray());
+  }
 }
--- grade.sh
+++ grade.sh
@@ -5,3 +5,26 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+if [! -f "ListExamples.java"]; then
+    echo "File does not exist";
+    exit
+    fi
+
+if [-f "ListExamples.java"]; then
+    echo "File does exist";
+    fi
+
+mkdir autoGrader
+cp ListExamples.java autoGrader/
+cp TestListExamples.java autoGrader/
+
+cd autoGrader
+cp lib/hamcrest-core-1.3.jar autoGrader
+cp lib/junit-4.13.2.jar autoGrader
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+if (!$?==0); then 
+    "Error in compiling";
+    set-e
